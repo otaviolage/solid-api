8cd8f2739e4aca4ded78e7c17dec54633200713e
otaviolage <otaviolagedossantos@gmail.com>
2021-08-31 00:00:42 -0300

    product entity and first service

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProdyctsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('A product with this name already exists');
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
